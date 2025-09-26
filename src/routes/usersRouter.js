8e0b3bcf3c1da67786705cc16f1eb25071d7619e
Valeria <valeria.bokotejova@gmail.com>
2025-09-26 20:55:38 +0200

    feat(users): add /users/:id/recipes & /users/:id/favorites + docs

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -2,6 +2,11 @@ import express from 'express';
 import authenticate from '../middlewares/authenticate.js';
 import { getCurrentUserController, getUserByIdController, uploadAvatarController } from '../controllers/usersControllers.js';
 import { upload } from '../middlewares/uploadAvatar.js';
+import express from 'express';
+import { getUserRecipes, getUserFavoriteRecipes } from '../controllers/usersController.js';
+import validateQuery from '../middlewares/validateQuery.js';
+import { listParamsSchema } from '../schemas/userRecipesSchemas.js';
+
 
 const usersRouter = express.Router();
 
@@ -10,5 +15,9 @@ usersRouter.get('/me', authenticate, getCurrentUserController);
 usersRouter.get('/:id', getUserByIdController);
 
 usersRouter.patch('/avatar', authenticate, upload.single('avatar'), uploadAvatarController);
+
+usersRouter.get('/:id/recipes', validateQuery(listParamsSchema), getUserRecipes);
+
+usersRouter.get('/:id/favorites', validateQuery(listParamsSchema), getUserFavoriteRecipes);
 
 export default usersRouter;
